be72f4c5f702cf3bb83a4901f6c93959772e5ef4
SamuraiLee0513007 <cyleeax@connect.ust.hk>
2023-06-07 06:57:42 -0400

    Lab changes

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ var Connection = require('tedious').Connection;
 var connection = new Connection(config);  
     connection.on('connect', function(err) {  
         if(err)
-        console.log("Error");  
+        console.log("Cannot connect to DB, ohno!");  
     });
     
 connection.connect();
